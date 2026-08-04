c68e5b502759633a9069ad7cb6644be23b83f6d4
admin <a123456@MacBook-Pro-cua-admin.local>
2026-08-04 17:29:55 +0700

    fix: export Express app directly for Vercel Serverless Function compatibility

--- api/index.js
+++ api/index.js
@@ -1,4 +1,3 @@
-import serverless from 'serverless-http';
 import { app } from '../backend/server.js';
 
-export default serverless(app);
+export default app;
--- backend/api/index.js
+++ backend/api/index.js
@@ -1,4 +1,3 @@
-import serverless from 'serverless-http';
 import { app } from '../server.js';
 
-export default serverless(app);
+export default app;
